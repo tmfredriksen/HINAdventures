dcd0ea760b13f8e154fbd93130ad703c1bbf6b25
tmfredriksen <tord.fredriksen@gmail.com>
2014-10-27 10:55:14 +0100

    First

---
+++ HINAdventures/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HINAdventures.Startup))]
+namespace HINAdventures
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
